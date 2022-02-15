6ef4f977d43af84c470ebc94146914346dddf78f
Artem Turov <58974140+Lunarelly@users.noreply.github.com>
2022-02-15 21:30:47 +0300

    1.0.0

---
+++ src/lunarelly/personaadapter/SkinAdapterPersona.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ *  _                               _ _
+ * | |   _   _ _ __   __ _ _ __ ___| | |_   _
+ * | |  | | | |  _ \ / _  |  __/ _ \ | | | | |
+ * | |__| |_| | | | | (_| | | |  __/ | | |_| |
+ * |_____\____|_| |_|\____|_|  \___|_|_|\___ |
+ *                                      |___/
+ *
+ * @author Lunarelly
+ * @link https://github.com/Lunarelly
+ *
+ */
+
+namespace lunarelly\personaadapter;
+
+use pocketmine\entity\Skin;
+use pocketmine\network\mcpe\convert\LegacySkinAdapter;
+use pocketmine\network\mcpe\protocol\types\skin\SkinData;
+
+class SkinAdapterPersona extends LegacySkinAdapter
+{
+
+    /**
+     * @param SkinData $data
+     * @return Skin
+     */
+    public function fromSkinData(SkinData $data): Skin
+    {
+        if ($data->isPersona()) {
+            return SkinLoader::getInstance()->getSkin();
+        }
+
+        return parent::fromSkinData($data);
+    }
+}
---
+++ src/lunarelly/personaadapter/SkinLoader.php
@@ -0,0 +1,101 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ *  _                               _ _
+ * | |   _   _ _ __   __ _ _ __ ___| | |_   _
+ * | |  | | | |  _ \ / _  |  __/ _ \ | | | | |
+ * | |__| |_| | | | | (_| | | |  __/ | | |_| |
+ * |_____\____|_| |_|\____|_|  \___|_|_|\___ |
+ *                                      |___/
+ *
+ * @author Lunarelly
+ * @link https://github.com/Lunarelly
+ *
+ */
+
+namespace lunarelly\personaadapter;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\entity\Skin;
+use pocketmine\network\mcpe\convert\SkinAdapterSingleton;
+
+class SkinLoader extends PluginBase
+{
+
+    /**
+     * @var SkinLoader|null
+     */
+    public static ?SkinLoader $instance = null;
+
+    /**
+     * @var Skin
+     */
+    private Skin $skin;
+
+    /**
+     * @return void
+     */
+    public function onEnable(): void
+    {
+        self::$instance = $this;
+
+        $this->saveResource("default.png", false);
+        $skinPaths = glob($this->getDataFolder() . "default.png");
+
+        foreach ($skinPaths as $skinPath) {
+            $image = imagecreatefrompng($skinPath);
+
+            if ($image === false) {
+                continue;
+            }
+
+            $this->skin = new Skin("skin." . basename($skinPath), $this->fromImage($image), "", "geometry.humanoid.custom");
+            @imagedestroy($image);
+        }
+
+        SkinAdapterSingleton::set(new SkinAdapterPersona());
+    }
+
+    /**
+     * @return SkinLoader
+     */
+    public static function getInstance(): SkinLoader
+    {
+        return self::$instance;
+    }
+
+    /**
+     * @param $image
+     * @return string
+     */
+    public function fromImage($image): string
+    {
+        $bytes = "";
+
+        for ($y = 0; $y < imagesy($image); $y++) {
+            for ($x = 0; $x < imagesx($image); $x++) {
+                $rgba = @imagecolorat($image, $x, $y);
+
+                $a = ((~($rgba >> 24)) << 1) & 0xff;
+                $r = ($rgba >> 16) & 0xff;
+                $g = ($rgba >> 8) & 0xff;
+                $b = $rgba & 0xff;
+
+                $bytes .= chr($r) . chr($g) . chr($b) . chr($a);
+            }
+        }
+        @imagedestroy($image);
+
+        return $bytes;
+    }
+
+    /**
+     * @return Skin
+     */
+    public function getSkin(): Skin
+    {
+        return $this->skin;
+    }
+}
